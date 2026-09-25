f8a255036cb9779efbb2bd33b8f2923b5628a9d6
carlosrojas88 <19867125@clases.edu.sv>
2026-09-25 17:25:42 -0600

    parte 2

---
+++ Ejercicio_de_guia_2.js
@@ -0,0 +1,73 @@
+const prompt = require('prompt-sync')();
+
+// ejercicio 1
+
+let numero1 = prompt("Ingrese el primer número: ");
+let numero2 = prompt("Ingrese el segundo número: ");
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+let suma = numero1 + numero2;
+
+console.log("La suma es: " + suma);
+
+// ejercicio 2
+
+let precio1 = prompt("Ingrese el precio del primer producto: ");
+let precio2 = prompt("Ingrese el precio del segundo producto: ");
+
+precio1 = Number(precio1);
+precio2 = Number(precio2);
+
+let precioT = precio1 + precio2;
+
+console.log("el precio total es: $" + precioT);
+
+// ejercicio 3
+
+let edad1 = prompt("Ingrese la primera edad: ");
+let edad2 = prompt("Ingrese la segunda edad: ");
+
+edad1 = Number(edad1);
+edad2 = Number(edad2);
+
+let edadt = numero1 + numero2;
+
+console.log("La edad total es: " + edadt);
+
+// ejercicio 4
+
+let nota1 = prompt("Ingrese la primera nota: ");
+let nota2 = prompt("Ingrese la segunda nota: ");
+
+nota1 = Number(nota1);
+nota2 = Number(nota2);
+
+let notat = nota1 + nota2;
+
+console.log("La nota total es: " + notat);
+
+// ejercicio 5
+
+let total1 = prompt("Ingrese el total de estudiantes de la primera aula: ");
+let total2 = prompt("Ingrese el total de estudiantes de la segunda aula: ");
+
+total1 = Number(total1);
+total2 = Number(total2);
+
+let totalt = total1 + total2;
+
+console.log("La suma de los estudiantes es: " + totalt);
+
+// ejercicio 6
+
+let money1 = prompt("Ingrese la cantidad de dinero que tiene en la targeta: ");
+let money2 = prompt("Ingrese la cantidad de dinero que tiene en la alcancia: ");
+
+money1 = Number(money1);
+money2 = Number(money2);
+
+let moneyt = money1 + money2;
+
+console.log("La cantidad de dinero que tiene ahorrado es: $" + moneyt);
--- Entrada_de_datos.js
+++ Entrada_de_datos.js
@@ -37,7 +37,7 @@ let carnet = estudiante === 'si'
 let tarifa = 1000;
 
 if (isNaN (edad3)){
-    console.log('error: debes ingresar un numero valido')
+    console.log('error: debes ingresar un numero valido.')
 } else {
     if (edad3 >= 65 || carnet === true) {
         tarifa = tarifa / 2;
@@ -55,7 +55,7 @@ if (isNaN (edad3)){
 let nota = Number(prompt('ingresa la nota asignada: '))
 
 if (isNaN (nota)){
-    console.log('error: debes ingresar un numero valido: ')
+    console.log('error: debes ingresar un numero valido.')
 } else {
     if (nota <=5){
         console.log('Reprobado')
@@ -75,8 +75,9 @@ if (isNaN (nota)){
 let promedio = Number(prompt('Ingrese el promedio general: '))
 let money = prompt(`ingresos mensuales (si/no): `);
 let ingresof = money === 'si'
+
 if (isNaN (promedio)){
-    console.log('error: debes ingresar un numero valido: ')
+    console.log('error: debes ingresar un numero valido.')
 } else {
     if (promedio >= 8 || ingresof === true) {
         console.log('Felicidades, has sido aceptado en la beca')
@@ -84,6 +85,69 @@ if (isNaN (promedio)){
         console.log('Felicidades, has sido aceptado en la beca')
     } else {
         console.log('No ha sido aceptado en la beca')
+    }
+}
+
+// ejercicio 4 simulador de cajero
+
+let saldo = 20000;
+let entrada = prompt(`Bienvenido al cajero. Su saldo actual es de ${saldo}. ¿Cuánto dinero desea retirar? `);
+let monto = Number(entrada);
+
+if (isNaN (monto)){
+    console.log('error: debes ingresar un numero valido.')
+} else {
+    if (monto <= 0) {
+        console.log("Error: El monto debe ser mayor a 0.");
+    } else if (monto > saldo) {
+        console.log("Error: Fondos insuficientes.");
+    } else if (monto % 100 !== 0) {
+        console.log("Error: El cajero solo entrega billetes de $100. El monto debe ser múltiplo de 100.");
+    } else {
+        saldo -= monto;
+        console.log(`Retiro exitoso. Ha retirado ${monto}. Su nuevo saldo disponible es: ${saldo}.`);
+    }
+}
+
+// ejercicio 5 control de acceso
+
+let hrs = Number(prompt('Buenas, ¿podria decirme que hora es? Favor usar el formato de 24 hrs. '))
+console.log(`Asi que son las ${hrs} hrs.`)
+let rol = prompt('Cual es su rol (Admin/Invitado/Sin_rol)')
+let entrar = rol === 'Admin'
+let entrar2 = rol === 'Invitado'
+
+if (isNaN (hrs)){
+    console.log("error: debes ingresar un numero valido.");
+} else {
+   if (hrs >= 1 && hrs <= 24 && entrar === true){
+        console.log('puede entrar sr Admin')
+    } else if (hrs >= 8 && hrs <= 18 && entrar2 === true){
+        console.log('puede entrar sr Invitado')
+    } else {
+        console.log('No puede entrar')
+    }
+}
+
+//ejercicio 6 cotizador de seguros de automovil
+
+let años = Number(prompt('¿Cuál es la edad del conductor? '));
+let antiguedad = Number(prompt('¿Cuántos años de antigüedad tiene el vehículo? '));
+
+if (isNaN(edad) || isNaN(antiguedad)) {
+    console.log('Error: debes ingresar números válidos.');
+} else {
+    if (años < 21) {
+        console.log('No es posible cotizar a menores de 21 años.');
+    } else {
+        let precio = 15000;
+        if (antiguedad < 5) {
+            precio += 5000;
+        }
+        if (años >= 65) {
+            precio -= 2000;
+        }
+        console.log(` Precio final: ${precio}`);
     }
 }
 
